407f235eae4a24c50b19469a93b3fd7cd1cabbee
shung1103 <shung1103@hanmail.net>
2024-10-10 11:04:30 +0900

    비관적 락 해결

--- src/main/java/org/hanghae99/company/product/repository/ProductRepository.java
+++ src/main/java/org/hanghae99/company/product/repository/ProductRepository.java
@@ -1,13 +1,7 @@
 package org.hanghae99.company.product.repository;
 
-import jakarta.persistence.LockModeType;
 import org.hanghae99.company.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-
-import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    Optional<Product> findById(Long id);
 }
